664c56939a294af1995eb4fdbdf06e0db882c193
Scorpio2317 <valery096@mail.ru>
2025-09-24 18:17:35 -0400

    Created project, added car model, and start doing Car Controller script

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,59 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public class Wheel
+{
+    public WheelCollider collider;
+    public WheelType wheelType;
+}
+
+
+[System.Serializable]
+public enum WheelType
+{
+    front, rear
+}
+
+public class CarController : MonoBehaviour
+{
+    public Wheel[] wheels;
+    public Vector2 moveInput;
+    public float powerMultiplier = 1;
+    public float maxSteer = 30, wheelBase = 2.5f, trackWidth = 1.5f;
+
+    public void OnMove(InputValue value)
+    {
+        //print(value.Get<Vector2>());
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var item in wheels)
+        {
+            item.collider.motorTorque = moveInput.y * powerMultiplier;
+        }
+
+        float steer = moveInput.x * maxSteer;
+        if (moveInput.x > 0)
+        {
+            wheels[0].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (trackWidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelBase));
+            wheels[1].collider.steerAngle = steer;
+        } else if (moveInput.x < 0)
+        {
+            wheels[0].collider.steerAngle = steer;
+            wheels[1].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (-trackWidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelBase));
+        }
+        else
+        {
+            wheels[0].collider.steerAngle = wheels[1].collider.steerAngle = 0;
+        }
+
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            wheels[i].collider.transform.localRotation = Quaternion.Euler(0, wheels[i].collider.steerAngle, 0);
+        }
+    }
+}
