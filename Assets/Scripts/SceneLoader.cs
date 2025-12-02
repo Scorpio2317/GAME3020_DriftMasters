67ba28c4ba3617fcae268156c3362d928ed88df8
Scorpio2317 <106897134+Scorpio2317@users.noreply.github.com>
2025-12-02 18:20:35 -0500

    Added a new track and track selection scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void NewGame()
+    public void MainMenu()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MapSelectiionScene");
+    }
+
+    public void RaceTrackSnowMountain()
+    {
+        SceneManager.LoadScene("GameMap1Scene");
+    }
+
+    public void RaceTrackMountain()
+    {
+        SceneManager.LoadScene("GameMap2Scene 1");
     }
 
     public void ExitGame()
